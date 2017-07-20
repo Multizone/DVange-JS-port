15ea55b1f8c86b075cd0edf4c3ca067423bc3547
baykalov-maxim <baykalov.maxim@gmail.com>
2017-07-21 00:04:47 +0300

    fixed bug with move camera, set center pozition window on screan

--- camera.js
+++ camera.js
@@ -6,17 +6,27 @@ class Camera {
 		this.mX = 0;
 		this.mY = 0;
 
+		this.addedX = document.body.clientWidth/2-500;
+		this.addedY = 100;
+
 		this.renderingObjs = [];
 	}
 
 	checkMove() {
-		if (this.mX > 900 && this.mX < 1000) this.x+=0.1;
-		if (this.mX < 100 && this.mX > 0) this.x-=0.1;
-		if (this.mY > 500 && this.mY < 600) this.y+=0.1;
-		if (this.mY < 100 && this.mY > 0) this.y-=0.1;
+		if (this.mX > this.addedX + 900 && this.mX < this.addedX + 1000) this.x+=0.1;
+		if (this.mX < this.addedX + 100 && this.mX > this.addedX + 0) this.x-=0.1;
+		if (this.mY > this.addedY + 500 && this.mY < this.addedY + 600) this.y+=0.1;
+		if (this.mY < this.addedY + 100 && this.mY > this.addedY + 0) this.y-=0.1;
 	}
 
 	render() {
+		let canvas = document.getElementById('canvas');
+
+		this.addedX = document.body.clientWidth/2-500;
+		canvas.style.position = 'absolute';
+		canvas.style.left = this.addedX +'px';
+		canvas.style.top = this.addedY +'px';
+
 		this.renderingObjs[0].render(this.x, this.y);
 		this.renderingObjs[1].render(this.x, this.y);
 		this.renderingObjs[2].render(this.x, this.y);
